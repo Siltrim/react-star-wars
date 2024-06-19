7a06751de3b876aac41a6a0dec76e733c74d45ea
Siltrim <rojerr410@gmail.com>
2024-06-19 12:53:57 +0500

    added GA to button next/previos

--- src/components/PeoplePage/PeopleNavigation/PeopleNavigation.jsx
+++ src/components/PeoplePage/PeopleNavigation/PeopleNavigation.jsx
@@ -3,18 +3,41 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import styles from './PeopleNavigation.module.css';
 import UiButton from '../../UI/UiButton/UiButton';
+import ReactGA from 'react-ga4';
 
 const PeopleNavigation = ({ getResource, prevPage, nextPage, counterPage }) => {
-  const handleChangeNext = () => getResource(nextPage);
-  const handleChangePrev = () => getResource(prevPage);
+  const handleChangeNext = () => {
+    ReactGA.event({
+      category: 'Персонажи',
+      action: 'Нажатие на кнопку Next',
+      label: `Страница ${counterPage + 1}`,
+    });
+    getResource(nextPage);
+  };
+  const handleChangePrev = () => {
+    ReactGA.event({
+      category: 'Персонажи',
+      action: 'Нажатие на кнопку Previous',
+      label: `Страница ${counterPage + 1}`,
+    });
+    getResource(prevPage);
+  };
   return (
     <>
       <div className={styles.container}>
         <Link to={`/people/?page=${counterPage - 1}`}>
-          <UiButton disabled={!prevPage} text="Previous" onClick={handleChangePrev}></UiButton>
+          <UiButton
+            disabled={!prevPage}
+            text="Previous"
+            onClick={handleChangePrev}
+          ></UiButton>
         </Link>
         <Link to={`/people/?page=${counterPage + 1}`}>
-          <UiButton disabled={!nextPage} text="Next" onClick={handleChangeNext}></UiButton>
+          <UiButton
+            disabled={!nextPage}
+            text="Next"
+            onClick={handleChangeNext}
+          ></UiButton>
         </Link>
       </div>
     </>
