c7ec92ac5fac16e8936c370f66a028fb8bf62312
Siltrim <rojerr410@gmail.com>
2024-06-20 12:42:49 +0500

    fix function

--- src/services/handleGoogleAnalytics.js
+++ src/services/handleGoogleAnalytics.js
@@ -1,7 +1,7 @@
 /* global gtag */
 
 export const onRouteChange = description => {
-  gtag('page_view', 'G-XXXXXXXXXX', {
+  gtag('page_view', 'G-LGCNLB68LD', {
     page_path: window.location.pathname,
     page_title: document.title,
     page_description: description,
